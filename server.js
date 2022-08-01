c5f317d68c1ea576b761a4c816fa4a99407d571b
Yolo-He <2684921675@qq.com>
2022-08-02 00:58:13 +0800

    update

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ connection.connect(err =>{
   else console.log('连接成功')
 });
 
+const dbOption = (sql,response)=>{
+  connection.query(sql,function(err,res){
+    return err? response.json({status:-1,message:"请求失败",err})
+    :response.json({status:1,message:"请求成功",data:res});
+  })
+}
+
 app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/',(request,response)=>{
@@ -31,42 +38,25 @@ app.post('/addClass',(request,response)=>{
   let {c_name, time, place, p_limit,nm_money} = request.body;
   let na_money = ((nm_money / 5) * 1.5).toFixed(2);
   let sql = `insert into classes(c_name, time, place, p_limit,nm_money,na_money) values ("${c_name}", "${time}", "${place}", ${p_limit},"${nm_money}","${na_money}")`;
-  
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功"})
-  })
+  return dbOption(sql,response);
 })
 
 app.post('/addDefault',(request,response)=>{
   let {u_id,u_name,c_id,time} = request.body;
   let sql = `insert into def (u_id, u_name, c_id, time) values ("${u_id}", "${u_name}", "${c_id}", "${time}")`;
-  
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功"})
-  })
+  return dbOption(sql,response);
 })
 
 app.get('/getMonClass',(request,response)=>{
   let {Mon} = request.query;
   let sql = `select * from classes where year(time) ="${Mon.substring(0,4)}" and month(time) ="${Mon.substring(6,7)}"`;
-
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功",data:res});
-  })
+  return dbOption(sql,response);
 })
 
 app.get('/getTodayClass',(request,response)=>{
   let {Today} = request.query;
-  
   let sql = `select * from classes where date_format(time,'%Y-%m-%d') = "${Today}"`;
-
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功",data:res});
-  })
+  return dbOption(sql,response);
 })
 
 app.get('/getSignupUsers',(request,response)=>{
@@ -75,22 +65,14 @@ app.get('/getSignupUsers',(request,response)=>{
   // let sql = `select * from signup where c_id =${c_id}`;
   let sql=`select s.id,s.c_id,s.c_name,s.u_id,s.u_name,s.appo_time,s.cost,time
   from signup as s left join def on s.u_id=def.u_id where s.c_id=${c_id}`;
-
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功",data:res});
-  })
+  return dbOption(sql,response);
 })
 
 app.get('/getClassById',(request,response)=>{
   let {c_id} = request.query;
   
   let sql = `select * from classes where c_id =${c_id}`;
-
-  connection.query(sql,function(err,res){
-    err? response.json({status:-1,message:"请求失败",err})
-    :response.json({status:1,message:"请求成功",data:res});
-  })
+  return dbOption(sql,response);
 })
 
 app.listen(8000,()=>{
