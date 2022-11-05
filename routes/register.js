a889cd636633c0ccb1854ea5a37e94a6903d2c3d
Yolo-10 <2684921675@qq.com>
2022-11-06 01:19:09 +0800

    注册加工号

--- routes/register.js
+++ routes/register.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const connection = require("../utils/db");
 
 router.post('/',(request,response)=>{
-  let {u_id,password} = request.body;
+  let {u_id,u_name,password} = request.body;
   let sql1 = `select * from users where u_id="${u_id}"`;
-  let sql2 = `insert into users values ("${u_id}","${u_id}","${password}",1)`;
+  let sql2 = `insert into users values ("${u_id}","${u_name}","${password}",1)`;
 
   connection.query(sql1,function(err,res){
     if(err) response.json({status:-1,message:"注册失败",err});
